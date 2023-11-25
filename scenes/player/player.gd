205a7d76386657b34daa66584d7c203d7c3d77bc
fortes <thomas.fortes@etu.unistra.fr>
2023-11-25 18:22:21 +0100

    ajout de l'ui phone

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -10,15 +10,16 @@ enum MoveDirection {DOWN,UP,RIGHT,LEFT}
 @export var move_speed : int
 @export var interacting = false
 @export var is_sitting : bool = false
-@export var state : State = State.IDLE 
+@export var state : State = State.IDLE
 @export var move_direction : MoveDirection = MoveDirection.DOWN
 var direction : Vector2
 var body_in_interact_range = null
 @export var friction = 300
 @export var cutscene = false
-@onready var stress = $player_hud/ui/stress_bar
-@onready var sante = $player_hud/ui/health_bar
+@onready var stress = $player_hud/HBoxContainer/ui/VBoxContainer/stress_bar
+@onready var sante = $player_hud/HBoxContainer/ui/VBoxContainer/health_bar
 @onready var player_hud = $player_hud
+@onready var phone = $player_hud/HBoxContainer/Phone
 
 
 func _ready():
@@ -30,6 +31,7 @@ func _ready():
 	GlobalScript.interaction_finished.connect(_on_interaction_finished)
 	GlobalScript.current_map.focus_on_map.connect(_on_focus_on_map_received)
 	camera.enabled = true
+	phone.visible = false
 
 	
 func _physics_process(delta):
@@ -46,6 +48,8 @@ func _physics_process(delta):
 			move_direction = MoveDirection.UP
 		elif Input.is_action_pressed("ui_down"):
 			move_direction = MoveDirection.DOWN
+		elif Input.is_action_pressed("open_interface"):
+			switch_interface()
 		
 		if not (velocity == Vector2(0,0)):
 			animate(State.MOVE)
@@ -58,7 +62,7 @@ func _physics_process(delta):
 			velocity = direction * move_speed
 			state = State.MOVE
 		else:
-			var deceleration = friction * delta 
+			var deceleration = friction * delta
 			velocity = velocity.move_toward(Vector2(0, 0), deceleration)
 			state = State.IDLE
 		move_and_slide()
@@ -69,7 +73,7 @@ func _physics_process(delta):
 	animate(state)
 	
 func animate(state : State):
-	var string_state: String 
+	var string_state: String
 	var string_move_direction : String;
 	
 	if state == State.MOVE:
@@ -101,7 +105,7 @@ func _on_area_2d_body_entered(body):
 
 func _on_area_2d_body_exited(body):
 	if body == body_in_interact_range:
-		body_in_interact_range = null 
+		body_in_interact_range = null
 
 
 func _on_emote_animation_finished():
@@ -117,3 +121,45 @@ func _on_focus_on_map_received():
 func _on_interaction_finished():
 	#interact_latency_timer.start()
 	pass
+
+
+func _on_btn_dr_1_pressed():
+	visible_info_dr("Info_dr1")
+
+
+func _on_btn_dr_2_pressed():
+	visible_info_dr("Info_dr2")
+
+
+func _on_btn_dr_3_pressed():
+	visible_info_dr("Info_dr3")
+
+
+func _on_btn_dr_4_pressed():
+	visible_info_dr("Info_dr4")
+
+
+func _on_btn_dr_5_pressed():
+	visible_info_dr("Info_dr5")
+
+
+func _on_btn_dr_6_pressed():
+	visible_info_dr("Info_dr6")
+	
+func visible_info_dr(current_dr : String):
+	for child in $player_hud/HBoxContainer/Phone/HBoxContainer/PanelContainer_Info.get_children():
+		if child is Panel:
+			child.visible=false
+	var node_path = "player_hud/HBoxContainer/Phone/HBoxContainer/PanelContainer_Info/" + current_dr
+	var target_node = get_node(node_path)
+	target_node.visible = true
+	
+func switch_interface():
+	if phone.visible == true:
+		phone.visible = false
+		interacting = true
+		interact_latency_timer.start()
+	else:
+		phone.visible = true
+		interacting = true
+		interact_latency_timer.start()
